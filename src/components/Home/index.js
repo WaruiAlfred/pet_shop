d9f940a0d4a4118b54bd6fa96dfdd55c1c9af54e
WaruiAlfred <alfredwarui17@gmail.com>
2023-05-20 23:28:51 +0300

    render IntroSection component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,12 +2,14 @@ import { MDBRow } from "mdb-react-ui-kit";
 import React, { Fragment } from "react";
 import styles from "./Home.module.css";
 import Navbar from "./Navbar";
+import IntroSection from "./IntroSection";
 
 const Home = () => {
   return (
     <Fragment>
-      <MDBRow className={`${styles.top_row} bg-dark bg-gradient`}>
+      <MDBRow className={`${styles.top_row} bg-dark bg-gradient `}>
         <Navbar />
+        <IntroSection />
       </MDBRow>
       <MDBRow className={styles.top_row}>Bottom Section</MDBRow>
     </Fragment>
