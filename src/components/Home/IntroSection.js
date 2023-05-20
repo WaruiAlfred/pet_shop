48965143e0cd237a80c70be547bd7487f557df1b
WaruiAlfred <alfredwarui17@gmail.com>
2023-05-20 23:28:22 +0300

    add Introsection with carousel

---
+++ src/components/Home/IntroSection.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { MDBCarousel, MDBCarouselItem, MDBBtn } from "mdb-react-ui-kit";
+import styles from "./Home.module.css";
+
+export default function IntroSection() {
+  return (
+    <MDBCarousel showControls showIndicators>
+      <MDBCarouselItem
+        className={`w-100 d-block ${styles.image}`}
+        itemId={1}
+        src="https://tlcpetfood.com/wp-content/uploads/2021/12/tlc-pet-food-home.jpg"
+        alt="..."
+      >
+        <h1>Care of Your Pets</h1>
+        <p>
+          We give our best of services to make sure your pets are comfortable.
+        </p>
+        <MDBBtn color="danger" rounded className="mb-3">
+          {" "}
+          Our Services
+        </MDBBtn>
+      </MDBCarouselItem>
+      <MDBCarouselItem
+        className={`w-100 d-block ${styles.image}`}
+        itemId={2}
+        src="https://www.edmonton.ca/sites/default/files/public-files/documents/PetLicenceService03.jpg"
+        alt="..."
+      >
+        {/* <h5>Second slide label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+      </MDBCarouselItem>
+      <MDBCarouselItem
+        className={`w-100 d-block ${styles.image}`}
+        itemId={3}
+        src="https://www.thehappychickencoop.com/wp-content/uploads/2021/10/Top-Parrot-Species-1.jpg"
+        alt="..."
+      >
+        {/* <h5>Third slide label</h5>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
+      </MDBCarouselItem>
+    </MDBCarousel>
+  );
+}
