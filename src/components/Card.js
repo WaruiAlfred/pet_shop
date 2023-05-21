0ba862ac5edf5da61d2de8e4e89fd779ce76f18a
WaruiAlfred <alfredwarui17@gmail.com>
2023-05-21 14:11:37 +0300

    create Custom card component

---
+++ src/components/Card.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+import styles from "./Card.module.css";
+
+export default function Card({ title, description, image }) {
+  return (
+    <MDBCard
+      className={` ${styles.glass_card} h-100 d-flex flex-column align-items-center justify-content-center`}
+    >
+      <MDBCardImage
+        src={image}
+        position="top"
+        alt="..."
+        // className={`${styles.card_image} w-50 h-50 mx-auto mt-3`}
+        className="h-70"
+      />
+      <MDBCardBody className="d-flex flex-column align-items-center justify-content-center">
+        <MDBCardTitle>{title}</MDBCardTitle>
+        <MDBCardText>{description}</MDBCardText>
+      </MDBCardBody>
+    </MDBCard>
+  );
+}
