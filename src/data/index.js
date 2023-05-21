34d67d2691d49ee3ca7427e3e07850fa5b05bf0e
WaruiAlfred <alfredwarui17@gmail.com>
2023-05-21 14:11:22 +0300

    add whatweDo data

---
+++ src/data/index.js
@@ -0,0 +1,29 @@
+export const whatWeDo = [
+  {
+    id: 1,
+    image: "https://www.tricountyanimalhospital.com/storage/app/media/8w.jpg",
+    title: "Vaccination",
+    description:
+      "We offer vaccination for your pet right at your home or office",
+  },
+  {
+    id: 2,
+    image:
+      "https://octanecdn.com/forevervetscom/Forever-Vets-Feb-2021-Vaccines-Needed-for-Dog-Grooming-min.jpg",
+    title: "Pet Grooming",
+    description: "Your pet will be groomed well and will look amazing",
+  },
+  {
+    id: 3,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8yQof7I7kFLMjceSBTweiHkt1t6MUZRwbmjxC3doVMuXivVg6CYT9Yqyv-m85PulrZj0&usqp=CAU",
+    title: "Walks and Care",
+    description: "Certified veterinary care for your pet. We are here to help",
+  },
+  {
+    id: 4,
+    image: "https://d2zp5xs5cp8zlg.cloudfront.net/image-46831-800.jpg",
+    title: "Teeth Cleaning",
+    description: "We offer teeth cleaning for your pet done by professionals",
+  },
+];
