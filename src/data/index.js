c1e697b26ab5067f108059e6cefcf9b040c3f0b2
WaruiAlfred <alfredwarui17@gmail.com>
2023-05-23 01:43:45 +0300

    add expert team data

--- src/data/index.js
+++ src/data/index.js
@@ -63,3 +63,34 @@ export const achievements = [
     duration: "2",
   },
 ];
+
+export const expertTeam = [
+  {
+    id: 1,
+    name: "Cynthia Car",
+    image:
+      "https://media.istockphoto.com/id/928227044/photo/beautiful-asian-woman-against-black-background.jpg?b=1&s=170667a&w=0&k=20&c=t3jI7hCBSrI0AWoyxYJKqmgUyz2a1E1I4R3tJw26c0I=",
+    designation: "Animal Caretaker",
+  },
+  {
+    id: 2,
+    name: "Jack Delgado",
+    image:
+      "https://media.istockphoto.com/id/1363870863/photo/happy-young-man-with-beard-looking-away-and-smiling.jpg?b=1&s=170667a&w=0&k=20&c=DXLL7YQ8Qm-q4l8mDrKpXQ3s7kQMZln9mXinmLcdsmQ=",
+    designation: "Veterinary Doctor",
+  },
+  {
+    id: 3,
+    name: "Adam Muli",
+    image:
+      "https://media.altphotos.com/cache/images/2017/07/06/06/752/black-man-portrait-cap.jpg",
+    designation: "Pet Trainer",
+  },
+  {
+    id: 4,
+    name: "Eugenio Kamau",
+    image:
+      "https://media.istockphoto.com/id/1363870213/photo/north-african-handsome-young-man-portrait.jpg?b=1&s=170667a&w=0&k=20&c=OYax58wt9OD4f4GbBW0Y08vgsCfqZzdGjSna_4wuhyA=",
+    designation: "Manager",
+  },
+];
