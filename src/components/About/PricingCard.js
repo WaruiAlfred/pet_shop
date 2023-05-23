197de2b355e37367a4be4d358b745ba4319fc402
WaruiAlfred <alfredwarui17@gmail.com>
2023-05-23 22:16:48 +0300

    create a PricingCard Component

---
+++ src/components/About/PricingCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardText,
+  MDBBtn,
+  MDBTypography,
+} from "mdb-react-ui-kit";
+import styles from "./PricingCard.module.css";
+
+export default function PricingCard({ title, price, description }) {
+  return (
+    <MDBCard className={`${styles.pricing_card}`}>
+      <MDBCardBody className="d-flex flex-column align-items-center justify-content-center">
+        <MDBTypography variant="h4">{title}</MDBTypography>
+        <MDBTypography variant="h1">&#36;{price}</MDBTypography>
+        {description.map((item) => (
+          <MDBCardText key={item.id}>{item.text}</MDBCardText>
+        ))}
+
+        <MDBBtn color="danger" rounded className={`${styles.buy_btn}`}>
+          Buy Now
+        </MDBBtn>
+      </MDBCardBody>
+    </MDBCard>
+  );
+}
