3a91bb46e0a33efa6e5c22e0711e4921917195c3
WaruiAlfred <alfredwarui17@gmail.com>
2023-05-24 11:52:32 +0300

    create pages components

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import About from "../components/About/index";
+
+function AboutPage() {
+  return <About />;
+}
+
+export default AboutPage;
---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Auth from "../components/Auth/index";
+
+function AuthPage() {
+  return <Auth />;
+}
+
+export default AuthPage;
