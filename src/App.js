edbb228b2e2142a7d2dcf64a31bb66814652a0ed
WaruiAlfred <alfredwarui17@gmail.com>
2023-05-24 12:11:43 +0300

    setup App to use router for navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { MDBContainer } from "mdb-react-ui-kit";
+import { MDBContainer, MDBRow } from "mdb-react-ui-kit";
 import Home from "./components/Home/index";
 import Auth from "./components/Auth/index";
 import About from "./components/About/index";
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <MDBContainer fluid className="px-0">
-      <Home />
+      {/* <Home /> */}
       {/* <Auth /> */}
       {/* <About /> */}
+      <MDBRow className={`bg-dark bg-gradient`}>
+        <Navbar />
+      </MDBRow>
+      <Outlet />
+      <Footer />
     </MDBContainer>
   );
 }
