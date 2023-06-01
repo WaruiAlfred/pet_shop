235a2eac9d80a5563e1e77ffdc461eb82eca2c7c
WaruiAlfred <alfredwarui17@gmail.com>
2023-06-01 23:31:17 +0300

    remove unnecessary import from App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import { MDBContainer, MDBRow } from "mdb-react-ui-kit";
-import Home from "./components/Home/index";
-import Auth from "./components/Auth/index";
-import About from "./components/About/index";
 import Navbar from "./components/Layout/Navbar";
 import Footer from "./components/Layout/Footer";
 import { Outlet } from "react-router-dom";
@@ -10,9 +7,6 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <MDBContainer fluid className="px-0">
-      {/* <Home /> */}
-      {/* <Auth /> */}
-      {/* <About /> */}
       <MDBRow className={`bg-dark bg-gradient`}>
         <Navbar />
       </MDBRow>
