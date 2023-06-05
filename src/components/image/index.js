15f92465aa9642675898b20470d4b71a39a907aa
WaruiAlfred <alfredwarui17@gmail.com>
2023-06-05 19:27:12 +0300

    implement react card flip

--- src/components/image/index.js
+++ src/components/image/index.js
@@ -1,14 +1,33 @@
-import React from "react";
-import { MDBRipple } from "mdb-react-ui-kit";
+import React, { useState } from "react";
+import { MDBRipple, MDBTypography } from "mdb-react-ui-kit";
+import ReactCardFlip from "react-card-flip";
+import styles from "./Image.module.css";
 
-export default function Image() {
+export default function ImageCard() {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped((prevState) => !prevState);
+  };
+
   return (
-    <MDBRipple rippleTag="a">
-      <img
-        src="https://mdbootstrap.com/img/new/standard/city/044.webp"
-        className="img-fluid rounded"
-        alt="example"
-      />
-    </MDBRipple>
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <MDBRipple rippleTag="a" onClick={handleClick} className="mb-3">
+        <img
+          src="https://mdbootstrap.com/img/new/standard/city/044.webp"
+          className="img-fluid rounded"
+          alt="example"
+        />
+      </MDBRipple>
+      <MDBRipple
+        rippleTag="a"
+        onClick={handleClick}
+        className="d-flex align-items-center justify-content-center mb-3 p-3"
+      >
+        <MDBTypography className="align-center">
+          Image description
+        </MDBTypography>
+      </MDBRipple>
+    </ReactCardFlip>
   );
 }
