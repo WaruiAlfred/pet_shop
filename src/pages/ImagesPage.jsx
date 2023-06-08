d458613dc5d56d2b66355c4b08b5b7d2e8069aaf
WaruiAlfred <alfredwarui17@gmail.com>
2023-06-09 00:15:23 +0300

    render images dynamically

--- src/pages/ImagesPage.jsx
+++ src/pages/ImagesPage.jsx
@@ -1,42 +1,17 @@
 import React from "react";
 import { MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
 import ImageCard from "../components/image/index";
+import { sampleImages } from "../data/index";
 
 const ImagesPage = () => {
   return (
     <MDBContainer fluid className="px-3 mt-3">
       <MDBRow className="mb-3">
-        <MDBCol md="4">
-          <ImageCard />
-        </MDBCol>
-        {/* </MDBRow>
-      <MDBRow className="mb-4"> */}
-        <MDBCol md="4">
-          <ImageCard />
-        </MDBCol>
-        <MDBCol md="4">
-          <ImageCard />
-        </MDBCol>
-        <MDBCol md="4">
-          <ImageCard />
-        </MDBCol>
-        <MDBCol md="4">
-          <ImageCard />
-        </MDBCol>
-        {/* </MDBRow>
-      <MDBRow className="mb-4"> */}
-        <MDBCol md="4">
-          <ImageCard />
-        </MDBCol>
-        <MDBCol md="4">
-          <ImageCard />
-        </MDBCol>
-        <MDBCol md="4">
-          <ImageCard />
-        </MDBCol>
-        <MDBCol md="4">
-          <ImageCard />
-        </MDBCol>
+        {sampleImages.map((image) => (
+          <MDBCol md="4" key={image.id}>
+            <ImageCard image={image.image} description={image.description} />
+          </MDBCol>
+        ))}
       </MDBRow>
     </MDBContainer>
   );
