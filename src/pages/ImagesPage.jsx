3754b89d50010533015d33cd6e3a6367eeb63079
WaruiAlfred <alfredwarui17@gmail.com>
2023-06-11 14:56:58 +0300

    add page description text

--- src/pages/ImagesPage.jsx
+++ src/pages/ImagesPage.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
+import { MDBCol, MDBContainer, MDBRow, MDBTypography } from "mdb-react-ui-kit";
 import ImageCard from "../components/image/index";
 import { sampleImages } from "../data/index";
 
 const ImagesPage = () => {
   return (
-    <MDBContainer fluid className="px-3 mt-3">
+    <MDBContainer fluid className="px-5 mt-3">
+      <MDBRow className="mb-5 d-flex justify-content-center align-items-center flex-column">
+        <MDBTypography tag="h1" variant="h1" className="text-center">
+          Some of the pets at our center
+        </MDBTypography>
+        <MDBTypography className="text-center text-muted">
+          Click on <b>image</b> to see what they have to say
+        </MDBTypography>
+      </MDBRow>
       <MDBRow className="mb-3">
         {sampleImages.map((image) => (
-          <MDBCol md="4" key={image.id}>
+          <MDBCol md="4" sm="6" xs="12" key={image.id}>
             <ImageCard image={image.image} description={image.description} />
           </MDBCol>
         ))}
