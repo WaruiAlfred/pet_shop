ab983e1245f799f8d173a946ad9fbe76ad37d05f
WaruiAlfred <alfredwarui17@gmail.com>
2023-06-11 14:49:11 +0300

    style image component

--- src/components/image/index.js
+++ src/components/image/index.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { MDBRipple, MDBTypography } from "mdb-react-ui-kit";
 import ReactCardFlip from "react-card-flip";
+import styles from "./Image.module.css";
 
-export default function ImageCard() {
+export default function ImageCard({ image, description }) {
   const [isFlipped, setIsFlipped] = useState(false);
 
   const handleClick = () => {
@@ -13,9 +14,9 @@ export default function ImageCard() {
     <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
       <MDBRipple rippleTag="a" onClick={handleClick} className="mb-3">
         <img
-          src="https://mdbootstrap.com/img/new/standard/city/044.webp"
-          className="img-fluid rounded"
-          alt="example"
+          src={image}
+          className={` ${styles.image} img-fluid rounded`}
+          alt="pet images"
         />
       </MDBRipple>
       <MDBRipple
@@ -23,8 +24,8 @@ export default function ImageCard() {
         onClick={handleClick}
         className="d-flex align-items-center justify-content-center mb-3 h-100 pt-5"
       >
-        <MDBTypography className="align-center">
-          Image description
+        <MDBTypography className="align-center text-dark">
+          {description}
         </MDBTypography>
       </MDBRipple>
     </ReactCardFlip>
