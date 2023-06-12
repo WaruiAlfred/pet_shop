7c8f1d457b8c2bb7be5d3ff4837d9c31d0df710c
WaruiAlfred <alfredwarui17@gmail.com>
2023-06-12 20:14:40 +0300

    render image title

--- src/components/image/index.js
+++ src/components/image/index.js
@@ -3,7 +3,7 @@ import { MDBRipple, MDBTypography } from "mdb-react-ui-kit";
 import ReactCardFlip from "react-card-flip";
 import styles from "./Image.module.css";
 
-export default function ImageCard({ image, description }) {
+export default function ImageCard({ image, description, title }) {
   const [isFlipped, setIsFlipped] = useState(false);
 
   const handleClick = () => {
@@ -22,8 +22,11 @@ export default function ImageCard({ image, description }) {
       <MDBRipple
         rippleTag="a"
         onClick={handleClick}
-        className="d-flex align-items-center justify-content-center mb-3 h-100 pt-5"
+        className="d-flex flex-column align-items-center justify-content-center mb-3 h-100 p-5"
       >
+        <MDBTypography className="align-center text-dark" variant="h3">
+          {title}
+        </MDBTypography>
         <MDBTypography className="align-center text-dark">
           {description}
         </MDBTypography>
