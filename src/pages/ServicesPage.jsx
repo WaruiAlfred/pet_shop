d80d6806c9f25e29afdcfa91c37e3d0c413ad7e8
WaruiAlfred <alfredwarui17@gmail.com>
2023-06-14 23:02:29 +0300

    add intro image to service page

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,7 +1,24 @@
+import { MDBRow } from "mdb-react-ui-kit";
 import React from "react";
 
 function ServicesPage() {
-  return <div>Services</div>;
+  return (
+    <>
+      <MDBRow className="mb-3">
+        <img
+          src="https://lh3.googleusercontent.com/eun3C9nrgHBtqI_x6hfQdf6rjBmfK-bwHe4Z_rdZj4hDH8V_wHTRU1jWlUGXCEP292kndI89btwfCNrdUDyilD0OZ-aDMGP_qYEK284PnA=s0"
+          className="img-fluid shadow-4"
+          alt="services intro image"
+          style={{
+            objectFit: "cover",
+            height: "60vh",
+            width: "100%",
+            objectPosition: "center",
+          }}
+        />
+      </MDBRow>
+    </>
+  );
 }
 
 export default ServicesPage;
