8e0650990e96c9bbe0a0df04108db41438d23755
WaruiAlfred <alfredwarui17@gmail.com>
2023-06-15 23:47:08 +0300

    configure scroll to top button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { MDBContainer, MDBRow } from "mdb-react-ui-kit";
 import Navbar from "./components/Layout/Navbar";
 import Footer from "./components/Layout/Footer";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "react-scroll-to-top";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
       </MDBRow>
       <Outlet />
+      <ScrollToTop smooth color="#DC4C64" top={50} />
       <Footer />
     </MDBContainer>
   );
